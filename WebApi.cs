1f62b971198220c002732aa66facc163808fe2fa
konnta0 <konnta0@gmail.com>
2026-02-10 00:24:14 +0900

    feat: web api sample

---
+++ WebApi.cs
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S dotnet run
+#:sdk Microsoft.NET.Sdk.Web
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello, World!");
+
+app.Run();
